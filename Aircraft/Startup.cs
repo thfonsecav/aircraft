70188c603ed0c0eb35ec5fabd317f4b5047e8e4d
christopher <christopher@DESKTOP-JJPQE1G>
2016-07-11 00:58:03 +0200

    Add project files.

---
+++ Aircraft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aircraft.Startup))]
+namespace Aircraft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
